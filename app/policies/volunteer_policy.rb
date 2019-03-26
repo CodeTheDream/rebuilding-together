94ba52adc9d1cb02c7833e882fd2dbb56d5c0281
kwasi008 <yjkwasi007@gmail.com>
2019-03-26 20:19:59 +0000

    delete later

---
+++ app/policies/volunteer_policy.rb
@@ -0,0 +1,12 @@
+class VolunteerPolicy
+ attr_reader :user, :volunteer
+
+ def initialize(user, volunteer)
+   @user = user
+   @volunteer = volunteer
+ end
+
+ def index?
+   user.admin?
+ end
+end
---
+++ db/migrate/20190315055139_create_volunteers.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class CreateVolunteers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :volunteers do |t|
+      t.string :picture
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :mobile_phone
+      t.date :birthdate
+      t.string :gender
+      t.string :city
+      t.string :state
+      t.string :employer
+      t.string :position
+      t.string :availability
+      t.string :skill
+      t.text :volunteer_notes
+      t.integer :user_id
+      t.timestamps
+    end
+  end
+end
