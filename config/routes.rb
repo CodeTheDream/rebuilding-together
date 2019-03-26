3c972c4e1478612d3fcb772a12963fad46a0f2eb
kwasi008 <yjkwasi007@gmail.com>
2019-03-26 20:42:05 +0000

    merge conflicts fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'welcome/homepage'
   devise_for :users
   resources :repairs
   root 'welcome#homepage'
+  resources :volunteers
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_20_192147) do
+ActiveRecord::Schema.define(version: 2019_03_26_134238) do
+
+  create_table "owners", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "first_name"
+    t.string "middle_name"
+    t.string "last_name"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.string "country"
+    t.string "year"
+    t.string "email"
+    t.string "phone"
+    t.string "em_contact_name"
+    t.string "em_contact_phone"
+    t.string "em_contact_relationship"
+    t.string "language"
+    t.string "howd_you_hear"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_owners_on_user_id", unique: true
+  end
 
   create_table "repairs", force: :cascade do |t|
     t.string "repair_type"
@@ -29,8 +52,29 @@ ActiveRecord::Schema.define(version: 2019_03_20_192147) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "volunteers", force: :cascade do |t|
+    t.string "picture"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "mobile_phone"
+    t.date "birthdate"
+    t.string "gender"
+    t.string "city"
+    t.string "state"
+    t.string "employer"
+    t.string "position"
+    t.string "availability"
+    t.string "skill"
+    t.text "volunteer_notes"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
