bdf81af292e36a79720b9b2954278d55fecc2579
Narayan Rai <narayanrai97@gmail.com>
2019-03-27 10:41:56 -0400

    Finish Owner Index action and its view

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,4 +1,20 @@
 class Owner < ApplicationRecord
   belongs_to :user
   has_many   :repairs
+
+  def upcase_fields
+    state.upcase!
+  end
+  def full_name
+    [first_name, middle_name, last_name].compact.join(' ')
+  end
+
+  def full_address
+    partial_address = [address, city, state].compact.join(', ')
+    [partial_address, zip].compact.join(' ')
+  end
+
+  def short_address
+    [city, state].compact.join(', ')
+  end
 end
