dbb930936b72971cb6f060ec34feb818b2cab5e7
kwasi008 <yjkwasi007@gmail.com>
2019-03-27 15:21:22 +0000

    updated with suggestions added

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'welcome/homepage'
+  root 'welcome#homepage'
   devise_for :users
   resources :repairs
-<<<<<<< HEAD
-  root 'welcome#homepage'
-=======
-  
->>>>>>> master
   resources :volunteers
 
 end
