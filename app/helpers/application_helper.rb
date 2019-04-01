2f41c464f26e6f893aac18861542f5315b35e6b3
Narayan Rai <narayanrai97@gmail.com>
2019-04-01 12:29:23 -0400

    Fix options_for_select for the form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,3 @@
 module ApplicationHelper
-  def language_options
-    [['English', 'English'], ['Spanish', 'Spanish'],
-      ['Chinese', 'Chinese'], ['Vietnamese', 'Vietnamese'], ['France', 'France']]
-  end
-
-  def em_contact_relationship_options
-    [['Father', 'Father'], ['Mother', 'Mother'],
-      ['Brother', 'Brother'], ['Sister', 'Sister'], ['Child', 'Child'], ['Friend', 'Friend'],
-      ['Spouse', 'Spouse'], ['Partner', 'Partner'], ['Assistant', 'Assistant'], ['Manager', 'Manager']]
-  end
-
-  def howd_you_hear_options
-    [['Our website', 'Our website'], ['Friend/Colleague', 'Friend/Colleague'],
-    ['Mailed invitation', 'Mailed invitation'], ['Emailed invitation', 'Emailed invitation'],
-    ['Radio', 'Radio'], ['Television', 'Television'], ['Internet search', 'Internet search'],['Other', 'Other']]
-  end
 
 end
