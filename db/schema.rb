55b69645f76360e7fb0816c20870811f06a478df
Tilneil <tilneil.gary@gmail.com>
2019-04-08 15:49:25 +0000

    switching branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,84 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_04_08_010157) do
+
+  create_table "owners", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "first_name"
+    t.string "middle_name"
+    t.string "last_name"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.string "country"
+    t.string "year"
+    t.string "phone"
+    t.string "em_contact_name"
+    t.string "em_contact_phone"
+    t.string "em_contact_relationship"
+    t.string "language"
+    t.string "howd_you_hear"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_owners_on_user_id", unique: true
+  end
+
+  create_table "repairs", force: :cascade do |t|
+    t.string "repair_type"
+    t.string "repair_notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "owner_id"
+    t.string "status", default: "pending"
+    t.index ["owner_id"], name: "index_repairs_on_owner_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "role"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "volunteers", force: :cascade do |t|
+    t.string "picture"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "mobile_phone"
+    t.date "birthdate"
+    t.string "gender"
+    t.string "city"
+    t.string "state"
+    t.string "employer"
+    t.string "position"
+    t.string "skill"
+    t.text "volunteer_notes"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "avail_sun_am"
+    t.string "avail_sun_pm"
+    t.string "avail_mon_am"
+    t.string "avail_mon_pm"
+    t.string "avail_tue_am"
+    t.string "avail_tue_pm"
+    t.string "avail_wed_am"
+    t.string "avail_wed_pm"
+    t.string "avail_thr_am"
+    t.string "avail_thr_pm"
+    t.string "avail_fri_am"
+    t.string "avail_fri_pm"
+    t.string "avail_sat_am"
+    t.string "avail_sat_pm"
+  end
 
 end
