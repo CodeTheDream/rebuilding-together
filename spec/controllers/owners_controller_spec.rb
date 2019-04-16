6e7ba2b16f89fb85c5e75f5ace0d6070b5de93ee
kwasi008 <yjkwasi007@gmail.com>
2019-04-16 21:50:59 +0000

    first ran test for repairs feature

--- spec/controllers/owners_controller_spec.rb
+++ spec/controllers/owners_controller_spec.rb
@@ -3,7 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe OwnersController, type: :controller do
+   include Devise::Test::ControllerHelpers
+   def setup
+   @request.env["devise.mapping"] = Devise.mappings[:user]
+   sign_in FactoryBot.create(:user)
+ end
   describe 'GET #index' do
+    before :each do
+     setup
+   end
     it 'returns http success' do
       get :index
       expect(response).to have_http_status(:success)
--- spec/features/repairs_spec.rb
+++ spec/features/repairs_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.feature "Repairs", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+include Warden::Test::Helpers
+    def setup
+     @user = FactoryBot.create(:user)
+     login_as(@user, :scope => :user)
+    end
+  it "should create repair", js: true do
+    setup
+    owner = FactoryBot.create(:owner, user_id: @user.id)
+    visit '/repairs'
+    save_screenshot('test1.png')
+    click_on 'Create New Repair'
+    save_screenshot("test2.png")
+    expect(page).to have_content("Repair Center")
+    fill_in "Repair Type", with: "Plumbing" 
+    fill_in "Repair Notes", with: "Water coming down"
+    click_on "Submit"
+    repair = Repair.first
+    expect(repair.repair_type).to eq("Plumbing")
+  end
 end
+
